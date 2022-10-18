6e622e40a86f291e78c1d04251b960f6585f13d0
Phat-BC27 <phatthuu2000@email.com>
2022-10-18 17:43:31 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//component
+function App() {
+  //JSX : JavaScript XML
+  return (
+    <div className="App">
+      <h1> hello</h1>
+    </div>
+  );
+}
+
+export default App;
