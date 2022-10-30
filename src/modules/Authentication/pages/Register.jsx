e830e0d62895739949b8b47469e58c2be2e6ab11
Phat-BC27 <phatthuu2000@email.com>
2022-10-30 17:57:07 +0700

    update

--- src/modules/Authentication/pages/Register.jsx
+++ src/modules/Authentication/pages/Register.jsx
@@ -4,7 +4,11 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       name: "",
       email: "",
@@ -14,11 +18,23 @@ const Register = () => {
       gender: null,
       role: "USER",
     },
+
+    //chế độ kích hoạt validation mặc định là onSubmit
+    mode: "onTouched",
   });
 
+  // đăng kí đúng
   const onSubmit = (values) => {
     console.log(values);
   };
+
+  // khi đăng kí nhưng sai validation nó sẽ chạy vào hàm :
+  // nhưng hàm này chỉ lấy được trong function
+  const onError = (error) => {
+    console.log(error);
+  };
+
+  // muốn lấy ở ngoài để hiển thị ra giao diện phải lấy từ hook useForm : formState
 
   // const onSubmit = async (values) => {
   //   const newValue = {...values,birthday:pickDate}
@@ -40,12 +56,54 @@ const Register = () => {
   return (
     <div>
       <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Account" {...register("name")} />
-        <input type="text" placeholder="Email" {...register("email")} />
-        <input type="text" placeholder="Password" {...register("password")} />
-        <input type="text" placeholder="Phone" {...register("phone")} />
-        <input type="text" placeholder="Birthday" {...register("birtday")} />
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <div>
+          <input
+            type="text"
+            placeholder="Account"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "tài khoản không được để trống",
+              },
+              minLength: {
+                value: 5,
+                message: " tài khoản phải từ 5 đến 20 kí tự",
+              },
+              maxLength: {
+                value: 20,
+                message: "tài khoản phải từ  5 đến 20 kí tự",
+              },
+            })}
+          />
+
+          {errors.name && <p> {errors.name.message}</p>}
+        </div>
+        <div>
+          {" "}
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email", {
+              required: { value: true, message: "Email không được để trống" },
+            })}
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Password"
+          {...register("password", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          {...register("phone", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Birthday"
+          {...register("birtday", { required: true })}
+        />
         <button>Register</button>
       </form>
     </div>
