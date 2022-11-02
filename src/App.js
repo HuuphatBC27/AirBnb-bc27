ae2420d74f7be2683adf142997a415267a4a83bc
Phat-BC27 <phatthuu2000@email.com>
2022-11-02 21:44:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
         <Route path="/" element={<Auth />}>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />/
+          <Route path="/register" element={<Register />} />
         </Route>
 
         <Route path="/user" element={<UserAdmin />} />
